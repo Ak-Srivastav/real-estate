3c86ddc47273b94c56c218bbd204fa54550790ae
Ak-Srivastav <106487556+Ak-Srivastav@users.noreply.github.com>
2024-05-14 23:54:58 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import path from "path";
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT} `);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
